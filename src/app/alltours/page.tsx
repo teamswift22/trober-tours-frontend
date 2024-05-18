fa0634eb2b37313311f4a5934b4a7805c0c898b5
JhayStark <jaamen5@gmail.com>
2024-05-18 07:52:02 +0000

    fix:suspense boundary

--- src/app/alltours/page.tsx
+++ src/app/alltours/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import Layout from "@/components/layout";
 import NavBar from "@/components/navbar";
 import { useGetToursQuery } from "@/lib/features/tours/toursApiSlice";
@@ -96,40 +96,42 @@ const ManageTours = () => {
   };
 
   return (
-    <div className="px-10">
-      <h2 className="text-2xl font-medium mb-4">Manage Tours</h2>
-      <NavBar
-        navArray={["All Tours", "Upcoming", "Completed", "Cancelled"]}
-        getActiveTab={handleTourClick}
-      />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {allTours?.tours?.map((tour: any) => (
-          <div
-            key={tour.id}
-            className="p-4 w-66 bg-white rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 "
-          >
-            <img
-              src={tour?.image || "/no-img.jpg"}
-              alt={tour.name}
-              className="w-full h-48 object-cover rounded-2xl mb-2"
-            />
-            <div className="flex flex-col">
-              <h3 className="text-lg font-semibold">{tour.name}</h3>
-              <p className="text-xs font-light">{tour.description}</p>
-              <div className="flex justify-between items-center mt-4">
-                <span className="text-gray-500">{`₵${tour.price}`}</span>
-                <button
-                  // onClick={() => handleTourClick(tour.name)}
-                  className="flex text-sm text-white bg-[#FA7454] px-6 py-2 rounded-md transition-colors duration-300"
-                >
-                  View
-                </button>
+    <Suspense>
+      <div className="px-10">
+        <h2 className="text-2xl font-medium mb-4">Manage Tours</h2>
+        <NavBar
+          navArray={["All Tours", "Upcoming", "Completed", "Cancelled"]}
+          getActiveTab={handleTourClick}
+        />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {allTours?.tours?.map((tour: any) => (
+            <div
+              key={tour.id}
+              className="p-4 w-66 bg-white rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 "
+            >
+              <img
+                src={tour?.image || "/no-img.jpg"}
+                alt={tour.name}
+                className="w-full h-48 object-cover rounded-2xl mb-2"
+              />
+              <div className="flex flex-col">
+                <h3 className="text-lg font-semibold">{tour.name}</h3>
+                <p className="text-xs font-light">{tour.description}</p>
+                <div className="flex justify-between items-center mt-4">
+                  <span className="text-gray-500">{`₵${tour.price}`}</span>
+                  <button
+                    // onClick={() => handleTourClick(tour.name)}
+                    className="flex text-sm text-white bg-[#FA7454] px-6 py-2 rounded-md transition-colors duration-300"
+                  >
+                    View
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
