ed56d5f1366f9375bd7d8e3869df8cc6fd0b9922
JhayStark <jaamen5@gmail.com>
2024-05-20 07:02:05 +0000

    chore:map component

---
+++ src/components/google-maps/MapComponent.tsx
@@ -0,0 +1,47 @@
+import React, { useCallback, useState } from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "100%",
+  height: "100%",
+};
+
+const center = {
+  lat: 5.678639599999999,
+  lng: -0.1764235,
+};
+
+const MapComponent = () => {
+  const [map, setMap] = useState(null);
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_PLACES_KEY as string,
+  });
+
+  const onLoad = useCallback(function callback(map: any) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback() {
+    setMap(null);
+  }, []);
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+};
+
+export default React.memo(MapComponent);
